b64275071f49d2829ab79e236657f7e441034c40
Librekiel <zoomag98@gmail.com>
2018-03-12 20:44:28 +0000

    lab2 done: revert_string.c and swap.c modified

--- src/revert_string/revert_string.c
+++ src/revert_string/revert_string.c
@@ -1,7 +1,16 @@
 #include "revert_string.h"
+#include <string.h>
+#include "../swap/swap.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 void RevertString(char *str)
 {
 	// your code here
+	printf("length is %d",strlen(str)/2);
+	for (int i=0; i<strlen(str)/2; i++)
+	{
+	    Swap(&str[i],&str[strlen(str)-1-i]);
+	}
 }
 
--- src/swap/swap.c
+++ src/swap/swap.c
@@ -3,4 +3,7 @@
 void Swap(char *left, char *right)
 {
 	// ваш код здесь
+	char buffer = *left;
+	*left = *right;
+	*right = buffer;
 }
